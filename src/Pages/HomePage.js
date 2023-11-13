41a54a38fa31f47d2984f74db213f39e15443abd
Emmanuelgithunt <emma2004ife@gmail.com>
2023-11-13 16:00:40 +0100

    worked on the fourth and fifth homepage

--- src/Pages/HomePage.js
+++ src/Pages/HomePage.js
@@ -2,6 +2,8 @@ import React from 'react'
 import HeroPage from '../InnerPages/HeroPage'
 import HeroPage2 from '../InnerPages/HeroPage2'
 import HeroPage3 from '../InnerPages/HeroPage3'
+import HeroPage4 from '../InnerPages/HeroPage4'
+import HeroPage5 from '../InnerPages/HeroPage5'
 
 const HomePage = () => {
   return (
@@ -9,6 +11,8 @@ const HomePage = () => {
       <HeroPage/>
       <HeroPage2/>
       <HeroPage3/>
+      <HeroPage4/>
+      <HeroPage5/>
     </div>
   )
 }
